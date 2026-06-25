e2abd9373115a7c6c019d378e03df0267e0e9842
Ranji <vanjarapuranjith69@gmail.com>
2026-06-25 22:27:05 +0530

    Initial Spring Boot setup

---
+++ labourlink/src/main/java/com/ranji/labourlink/LabourlinkApplication.java
@@ -0,0 +1,13 @@
+package com.ranji.labourlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabourlinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabourlinkApplication.class, args);
+	}
+
+}
---
+++ labourlink/src/test/java/com/ranji/labourlink/LabourlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ranji.labourlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabourlinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
