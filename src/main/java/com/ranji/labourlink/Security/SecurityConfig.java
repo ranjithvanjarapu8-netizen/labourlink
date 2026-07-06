aa9e7b2688ed59c8c1919ff09461cc949eb3ddc5
Ranji <vanjarapuranjith69@gmail.com>
2026-07-06 09:19:52 +0530

    Allow Render website

--- src/main/java/com/ranji/labourlink/Security/SecurityConfig.java
+++ src/main/java/com/ranji/labourlink/Security/SecurityConfig.java
@@ -59,14 +59,11 @@ public class SecurityConfig {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of(
-                "http://127.0.0.1:5500",
-                "http://127.0.0.1:5501",
-                "http://localhost:5500",
-                "http://localhost:5501",
+        configuration.setAllowedOriginPatterns(List.of(
+                "http://localhost:*",
+                "http://127.0.0.1:*",
                 "https://labourlink-website.onrender.com"
         ));
-
         configuration.setAllowedMethods(List.of(
                 "GET",
                 "POST",
